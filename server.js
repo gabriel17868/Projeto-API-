6e8b12e6e3ff494067b0555e3109ee914956d91e
gabriel17868 <gabrielmaraschin123@gmail.com>
2025-10-23 16:50:45 -0300

    crud criado

--- server.js
+++ server.js
@@ -1,19 +1,58 @@
-import express, { request, response } from 'express'
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
 
-const users = []
 
-app.post('/usuarios', (req, res) => {
-    
-    users.push(req.body)
+app.post('/usuarios', async (req, res) => {
+
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
 
     res.status(201).json(req.body)
 })
 
-app.get('/usuarios', (req, res) => {
+app.put('/usuarios/:id', async (req, res) => {
+
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+})
+
+app.get('/usuarios', async (req, res) => {
+
+        console.log(req)
+
+    const users = await prisma.user.findMany()
+
     res.status(200).json(users)
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(201).json({ message: "Usuário deletado com Sucesso!" })
 })
 
 app.listen(3000)
@@ -25,4 +64,8 @@ app.listen(3000)
     - Listar todos os usuários: OK
     - Editar um usuário
     - Deletar um usuário
+
+    MongoDB
+    user: gabrielmaraschin
+    password: HgmelMLe6lg7jQoX
 */
